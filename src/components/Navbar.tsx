990570b9a9242ba864b8bec56b09a04f2907e8b2
Kotlia <62182668+Kotlia@users.noreply.github.com>
2021-06-11 21:50:58 -0400

    style: made parent responsible for width for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,13 @@
 import { useEffect, useState } from 'react'
 import styles from 'styles/Navbar.module.scss'
-import { desktopQuery, Desktop } from 'common/Responsive'
-import { useMediaQuery } from 'react-responsive'
+import { Desktop } from 'common/Responsive'
 import { Squeeze as Hamburger } from 'hamburger-react'
-
 import { client } from 'common/Prismic'
 import { RichText } from 'prismic-reactjs'
 
 export default function Navbar() {
-  const isDesktop = useMediaQuery(desktopQuery)
   const [isMenuOpen, setMenuOpen] = useState(false)
   const [items, setItems] = useState([])
-  const [topbarWidth, setTopbarWidth] = useState(styles.mainWidth)
   useEffect(() => {
     client.getSingle(`contents`, {}).then(r => {
       setItems(
@@ -23,58 +19,44 @@ export default function Navbar() {
         )
       )
     })
-    function onResize() {
-      setTopbarWidth(
-        window.innerWidth > window.screen.width / 2
-          ? `${window.screen.width / 2}px`
-          : styles.mainWidth
-      )
-    }
-    onResize()
-    window.addEventListener(`resize`, onResize)
-    return () => window.removeEventListener(`resize`, onResize)
   }, [])
   return (
-    <div
-      className={styles.main}
-      style={{
-        width: topbarWidth,
-        height: `${isDesktop ? 15 : 10}vh`
-      }}
-    >
-      {/* ロゴ */}
-      <div className={styles.logo}>
-        <img src='logo.svg' alt='logo' />
-        <div className={styles.caption}>
-          <p>Megumi</p>
-          <p>Church</p>
+    <>
+      <div className={styles.main}>
+        {/* ロゴ */}
+        <div className={styles.logo}>
+          <img src='logo.svg' alt='logo' />
+          <div className={styles.caption}>
+            <p>Megumi</p>
+            <p>Church</p>
+          </div>
+        </div>
+
+        {/* メニュー上部のショートカット */}
+        <div className={styles.shortcuts}>
+          {items.slice(0, 4).map((item: string) => {
+            const [name, route] = item.split(`@`)
+            return (
+              <Desktop>
+                <a className='underline' href={`/${route}`}>
+                  {name}
+                </a>
+              </Desktop>
+            )
+          })}
+        </div>
+
+        {/* メニューを開閉するボタン */}
+        <div
+          className={styles.menuIcon}
+          onMouseDown={() => {
+            setMenuOpen(!isMenuOpen)
+          }}
+        >
+          <p>Menu</p>
+          <Hamburger size={27} toggled={isMenuOpen} distance='sm' />
         </div>
       </div>
-
-      {/* メニュー上部のショートカット */}
-      <div className={styles.shortcuts}>
-        {items.slice(0, 4).map((item: string) => {
-          const [name, route] = item.split(`@`)
-          return (
-            <Desktop>
-              <a className='underline' href={`/${route}`}>
-                {name}
-              </a>
-            </Desktop>
-          )
-        })}
-      </div>
-
-      {/* メニューを開閉するボタン */}
-      <div
-        className={styles.menuIcon}
-        onMouseDown={() => {
-          setMenuOpen(!isMenuOpen)
-        }}
-      >
-        <p>Menu</p>
-        <Hamburger size={27} toggled={isMenuOpen} distance='sm' />
-      </div>
-    </div>
+    </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
 import Navbar from 'components/Navbar'
+import { useEffect, useState } from 'react'
 import styles from '~/src/styles/Home.module.scss'
 
 export default function Home() {
+  const [width, setWidth] = useState(`95vw`)
+  useEffect(() => {
+    function onResize() {
+      setWidth(
+        window.innerWidth > window.screen.width / 2
+          ? `${window.screen.width / 2}px`
+          : styles.mainWidth
+      )
+    }
+    onResize()
+    window.addEventListener(`resize`, onResize)
+    return () => window.removeEventListener(`resize`, onResize)
+  })
   return (
-    <div className={styles.common}>
+    <div
+      className={styles.common}
+      style={{
+        width
+      }}
+    >
       <Navbar />
     </div>
   )
