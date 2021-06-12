bdd39dce2583f3141f3029c84f1a0cce4e6cdac2
Kotlia <62182668+Kotlia@users.noreply.github.com>
2021-06-12 10:19:29 -0400

    feat(navbar): add menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from 'react'
 import styles from 'styles/Navbar.module.scss'
-import { Desktop } from 'common/Responsive'
+import { Desktop, desktopQuery } from 'common/Responsive'
 import { Squeeze as Hamburger } from 'hamburger-react'
 import { client } from 'common/Prismic'
 import { RichText } from 'prismic-reactjs'
+import Page, { use100vh } from 'react-div-100vh'
+import { useMediaQuery } from 'react-responsive'
 
-export default function Navbar() {
+export default function Navbar({ id }: { id?: string }) {
   const [isMenuOpen, setMenuOpen] = useState(false)
   const [items, setItems] = useState([])
+  const [topbarHeight, setTopbarHeight] = useState(0)
+  const padding = `${useMediaQuery(desktopQuery) ? 2 : 4}%`
   useEffect(() => {
+    setTopbarHeight(document.getElementById(`navbar`)!.clientHeight)
     client.getSingle(`contents`, {}).then(r => {
       setItems(
         r.data.contents.element.value.map(
@@ -21,42 +26,57 @@ export default function Navbar() {
     })
   }, [])
   return (
-    <div className={styles.main}>
-      <div className={styles.navbar}>
-        {/* ロゴ */}
-        <div className={styles.logo}>
-          <img src='logo.svg' alt='logo' />
-          <div className={styles.caption}>
-            <p>Megumi</p>
-            <p>Church</p>
-          </div>
-        </div>
-
-        {/* メニュー上部のショートカット */}
-        <div className={styles.shortcuts}>
-          {items.slice(0, 4).map((item: string) => {
-            const [name, route] = item.split(`@`)
-            return (
-              <Desktop>
-                <a className='underline' href={`/${route}`}>
-                  {name}
-                </a>
-              </Desktop>
-            )
-          })}
-        </div>
-
-        {/* メニューを開閉するボタン */}
+    <Page className={styles.main}>
+      <div id={id} className={styles.navbar}>
         <div
-          className={styles.menuIcon}
-          onMouseDown={() => {
-            setMenuOpen(!isMenuOpen)
+          className={styles.inner}
+          style={{
+            paddingTop: padding,
+            paddingBottom: padding
           }}
         >
-          <p>Menu</p>
-          <Hamburger size={27} toggled={isMenuOpen} distance='sm' />
+          {/* ロゴ */}
+          <div className={styles.logo}>
+            <img src='logo.svg' alt='logo' />
+            <div className={styles.caption}>
+              <p>Megumi</p>
+              <p>Church</p>
+            </div>
+          </div>
+
+          {/* メニュー上部のショートカット */}
+          <div className={styles.shortcuts}>
+            {items.slice(0, 4).map((item: string) => {
+              const [name, route] = item.split(`@`)
+              return (
+                <Desktop>
+                  <a className='underline' href={`/${route}`}>
+                    {name}
+                  </a>
+                </Desktop>
+              )
+            })}
+          </div>
+
+          {/* メニューを開閉するボタン */}
+          <div
+            className={styles.menuIcon}
+            onMouseDown={() => {
+              setMenuOpen(!isMenuOpen)
+            }}
+          >
+            <p>Menu</p>
+            <Hamburger size={27} toggled={isMenuOpen} distance='sm' />
+          </div>
         </div>
       </div>
-    </div>
+      <div className={styles.menu}>
+        1. メニューテキスト{`\n`}
+        2. メニューテキスト{`\n`}
+        3. メニューテキスト{`\n`}
+        4. メニューテキスト{`\n`}
+        5. メニューテキスト{`\n`}
+      </div>
+    </Page>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
 import Navbar from 'components/Navbar'
 import { useEffect, useState } from 'react'
 import { useMediaQuery } from 'react-responsive'
+import Page, { use100vh } from 'react-div-100vh'
 import styles from '~/src/styles/Home.module.scss'
 import { desktopQuery } from '~/src/common/Responsive'
 
 export default function Home() {
-  const [width, setWidth] = useState(`95vw`)
+  const [bodyWidth, setBodyWidth] = useState(`95vw`)
+  const [topbarHeight, setTopbarHeight] = useState(0)
   const isDesktop = useMediaQuery(desktopQuery)
   useEffect(() => {
+    setTopbarHeight(document.getElementById(`navbar`)!.clientHeight)
     function onResize() {
-      setWidth(
-        window.innerWidth > window.screen.width / 2 && isDesktop
+      const scrWidth = window.screen.width
+      setBodyWidth(
+        window.innerWidth > scrWidth / 2 + scrWidth * 0.025 && isDesktop
           ? `${window.screen.width / 2}px`
           : `95vw`
       )
@@ -20,11 +24,8 @@ export default function Home() {
     return () => window.removeEventListener(`resize`, onResize)
   })
   return (
-    <main className={styles.main} style={{ width }}>
-      <div className={styles.navbar}>
-        <Navbar />
-      </div>
-      <div className={styles.body} />
+    <main className={styles.main} style={{ width: bodyWidth }}>
+      <Navbar id='navbar' />
     </main>
   )
 }
