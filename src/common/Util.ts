720e8eddd939f365faf046251330f5ad5eefb6bd
Kotlia <62182668+Kotlia@users.noreply.github.com>
2021-06-13 10:15:41 -0400

    fix(navbar): fix body position

---
+++ src/common/Util.ts
@@ -0,0 +1,2 @@
+export const capitalizeFirstLetter = (s: string) =>
+  s[0].toUpperCase() + s.substring(1)
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,14 +4,25 @@ import { Desktop, desktopQuery } from 'common/Responsive'
 import { Squeeze as Hamburger } from 'hamburger-react'
 import { client } from 'common/Prismic'
 import { RichText } from 'prismic-reactjs'
-import Page, { use100vh } from 'react-div-100vh'
+import Page from 'react-div-100vh'
 import { useMediaQuery } from 'react-responsive'
+import { capitalizeFirstLetter } from '~/src/common/Util'
 
 export default function Navbar({ id }: { id?: string }) {
   const [isMenuOpen, setMenuOpen] = useState(false)
   const [items, setItems] = useState([])
-  const padding = `${useMediaQuery(desktopQuery) ? 2 : 4}%`
+  const [topbarHeight, setTopbarHeight] = useState(0)
+  const [windowHeight, setWindowHeight] = useState(0)
+  const [menuWidth, setMenuWidth] = useState(0)
+  const [mainWidth, setMainWidth] = useState(0)
+  const [menuDirection, setMenuDirection] = useState<'right' | 'left'>(`left`)
+  const isDesktop = useMediaQuery(desktopQuery)
+  const padding = `${isDesktop ? 2 : 4}%`
   useEffect(() => {
+    setTopbarHeight(document.getElementById(`navbar`)!.clientHeight)
+    setWindowHeight(document.getElementById(`100vh`)!.clientHeight)
+    setMenuWidth(document.getElementById(`menusection`)!.clientWidth)
+    setMainWidth(document.getElementById(`main`)!.clientWidth)
     client.getSingle(`contents`, {}).then(r => {
       setItems(
         r.data.contents.element.value.map(
@@ -22,10 +33,31 @@ export default function Navbar({ id }: { id?: string }) {
         )
       )
     })
+    function onResize() {
+      setTopbarHeight(document.getElementById(`navbar`)!.clientHeight)
+      setWindowHeight(document.getElementById(`100vh`)!.clientHeight)
+      setMenuWidth(document.getElementById(`menusection`)!.clientWidth)
+      setMainWidth(document.getElementById(`main`)!.clientWidth)
+    }
+    onResize()
+    window.addEventListener(`resize`, onResize)
+    return () => window.removeEventListener(`resize`, onResize)
   }, [])
   return (
-    <Page className={styles.main}>
-      <div id={id} className={styles.navbar}>
+    <Page
+      id='100vh'
+      className={styles.main}
+      style={{
+        width: mainWidth
+      }}
+    >
+      <div
+        id={id}
+        className={styles.navbar}
+        style={{
+          width: isDesktop ? `100%` : `90%`
+        }}
+      >
         <div
           className={styles.inner}
           style={{
@@ -60,7 +92,14 @@ export default function Navbar({ id }: { id?: string }) {
           <div
             className={styles.menuIcon}
             onMouseDown={() => {
+              console.log(menuWidth)
               setMenuOpen(!isMenuOpen)
+              if (isMenuOpen) {
+                setMenuDirection(menuDirection === `right` ? `left` : `right`)
+                setTimeout(() => {
+                  setMenuDirection(`left`)
+                }, 500)
+              }
             }}
           >
             <p>Menu</p>
@@ -68,12 +107,32 @@ export default function Navbar({ id }: { id?: string }) {
           </div>
         </div>
       </div>
-      <div className={styles.menu}>
-        1. メニューテキスト{`\n`}
-        2. メニューテキスト{`\n`}
-        3. メニューテキスト{`\n`}
-        4. メニューテキスト{`\n`}
-        5. メニューテキスト{`\n`}
+      <div
+        id='menusection'
+        className={styles.menuSection}
+        style={{
+          height: `${(1 - topbarHeight / windowHeight) * 100}%`
+        }}
+      >
+        <div
+          className={styles.menu}
+          style={{
+            width: `${isMenuOpen ? 100 : 0}%`,
+            ...Object.fromEntries([
+              [`margin${capitalizeFirstLetter(menuDirection)}`, `auto`]
+            ])
+          }}
+        >
+          <div
+            className={styles.top}
+            style={{
+              width: `${isDesktop ? 20 : 100}%`,
+              ...Object.fromEntries([
+                [`margin${capitalizeFirstLetter(menuDirection)}`, `auto`]
+              ])
+            }}
+          />
+        </div>
       </div>
     </Page>
   )
