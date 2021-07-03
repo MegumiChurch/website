08c5897f0d327d3176abef9eb69ea18254ff70f7
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-07-03 11:04:12 -0400

    feat: add title

--- src/pages/article/[id].tsx
+++ src/pages/article/[id].tsx
@@ -6,6 +6,7 @@ import styles from 'styles/[id].module.scss'
 import { useRouter } from 'next/router'
 import Navbar from 'components/Navbar'
 import { client } from 'common/Prismic'
+import Head from 'next/head'
 
 interface Document {
   header: string
@@ -37,38 +38,45 @@ export default function id() {
       })
   }, [isReady])
   return (
-    <div className={styles.root}>
-      <Navbar />
-      <div className={`${styles.main} center`}>
-        {doc?.body ? (
-          <>
-            <div
-              className={styles.header}
-              style={{
-                zIndex: -1,
-                backgroundImage: `url("${doc.header}")`
-              }}
-            />
-            <div className={styles.text}>
-              <span className={styles.title}>{RichText.asText(doc.title)}</span>
-              <div className={styles.info}>
-                <span className={styles.author}>
-                  {RichText.asText(doc.author)}
+    <>
+      <Head>
+        <title>{`${RichText.asText(doc?.title || [])} - NY めぐみ教会`}</title>
+      </Head>
+      <div className={styles.root}>
+        <Navbar />
+        <div className={`${styles.main} center`}>
+          {doc?.body ? (
+            <>
+              <div
+                className={styles.header}
+                style={{
+                  zIndex: -1,
+                  backgroundImage: `url("${doc.header}")`
+                }}
+              />
+              <div className={styles.text}>
+                <span className={styles.title}>
+                  {RichText.asText(doc.title)}
                 </span>
-                <span className={styles.date}>
-                  最終更新：
-                  {doc.lastUpdate.substring(0, 10).replaceAll(`-`, `.`)}
-                </span>
+                <div className={styles.info}>
+                  <span className={styles.author}>
+                    {RichText.asText(doc.author)}
+                  </span>
+                  <span className={styles.date}>
+                    最終更新：
+                    {doc.lastUpdate.substring(0, 10).replaceAll(`-`, `.`)}
+                  </span>
+                </div>
+                <div className={styles.body}>
+                  <RichText render={doc.body} />
+                </div>
               </div>
-              <div className={styles.body}>
-                <RichText render={doc.body} />
-              </div>
-            </div>
-          </>
-        ) : (
-          <p>Loading...</p>
-        )}
+            </>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,70 +1,76 @@
 import styles from 'styles/Home.module.scss'
 import Navbar from 'components/Navbar'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className={styles.root}>
-      <Navbar />
-      <main className='center'>
-        <div className={styles.windowTop}>
-          <div className={styles.filter}>
-            <div className={styles.text}>
-              <p>NY めぐみ教会</p>
-              <p>Megumi Church</p>
-            </div>
-          </div>
-        </div>
-        <div className={styles.body}>
-          <div className={styles.first} id='first'>
-            <div className='verticalCenterChild'>
-              <div>
-                <h2>
-                  <span>毎週</span>
-                  <span>日曜日</span>
-                  <span>午前</span>
-                  <span>九時</span>
-                  <span>より、</span>
-                  <span>リッジウェイ</span>
-                  <span>教会</span>
-                  <span>地下</span>
-                  <span>グリーン</span>
-                  <span>・</span>
-                  <span>ルーム</span>
-                  <span>にて。</span>
-                  <span className={styles.eng}>Zoom</span>
-                  <span>で</span>
-                  <span>参加も</span>
-                  <span>できます。</span>
-                </h2>
-                <div className={`${styles.dock}`}>
-                  <img
-                    className='clickAble'
-                    src='googleMap.webp'
-                    alt=''
-                    onMouseDown={() => {
-                      window.location.href = `https://goo.gl/maps/dAQFo16Biq41pc2S9`
-                    }}
-                  />
-                  <img
-                    className='clickAble'
-                    src='zoom.webp'
-                    alt=''
-                    onMouseDown={() => {
-                      window.location.href = `https://us02web.zoom.us/j/4256468662`
-                    }}
-                  />
-                </div>
+    <>
+      <Head>
+        <title>NY めぐみ教会</title>
+      </Head>
+      <div className={styles.root}>
+        <Navbar />
+        <main className='center'>
+          <div className={styles.windowTop}>
+            <div className={styles.filter}>
+              <div className={styles.text}>
+                <p>NY めぐみ教会</p>
+                <p>Megumi Church</p>
               </div>
             </div>
           </div>
-          <div className={styles.second} id='second'>
-            <div>
-              <div />
-              <div />
+          <div className={styles.body}>
+            <div className={styles.first} id='first'>
+              <div className='verticalCenterChild'>
+                <div>
+                  <h2>
+                    <span>毎週</span>
+                    <span>日曜日</span>
+                    <span>午前</span>
+                    <span>九時</span>
+                    <span>より、</span>
+                    <span>リッジウェイ</span>
+                    <span>教会</span>
+                    <span>地下</span>
+                    <span>グリーン</span>
+                    <span>・</span>
+                    <span>ルーム</span>
+                    <span>にて。</span>
+                    <span className={styles.eng}>Zoom</span>
+                    <span>で</span>
+                    <span>参加も</span>
+                    <span>できます。</span>
+                  </h2>
+                  <div className={`${styles.dock}`}>
+                    <img
+                      className='clickAble'
+                      src='googleMap.webp'
+                      alt=''
+                      onMouseDown={() => {
+                        window.location.href = `https://goo.gl/maps/dAQFo16Biq41pc2S9`
+                      }}
+                    />
+                    <img
+                      className='clickAble'
+                      src='zoom.webp'
+                      alt=''
+                      onMouseDown={() => {
+                        window.location.href = `https://us02web.zoom.us/j/4256468662`
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.second} id='second'>
+              <div>
+                <div />
+                <div />
+              </div>
             </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </>
   )
 }
