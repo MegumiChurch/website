9a665a19491a82cca50a7b15256c19415037f8ce
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-07-11 07:30:12 -0400

    style: add stylelint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-import Head from 'next/head';
-import Image from 'next/image';
-
-import styles from '~/src/styles/home.module.scss';
 import Layout from '~/src/components/layout';
 
 export default function Home() {
