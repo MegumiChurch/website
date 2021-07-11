b0952c0a0beb34c4f244f6f3d2ce1a1b554fb083
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-07-11 17:13:40 -0400

    feat(menu): add menu

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,7 @@
-import { ReactChild } from 'react';
+import { MutableRefObject, ReactChild, useRef, useState } from 'react';
 import { Cross as Hamburger } from 'hamburger-react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from './layout.module.scss';
 import Footer from '~/src/components/footer';
 
@@ -8,20 +10,48 @@ interface Props {
 }
 
 export default function Layout({ children }: Props) {
+  const ref = useRef() as MutableRefObject<HTMLDivElement>;
+  const [isOpen, setOpen] = useState(false);
   return (
     <>
-      <div className={styles.controls}>
-        <div className={styles.menuButton}>
-          <div className={styles.back} />
-          <Hamburger color="#FFF" />
+      <>
+        <div className={styles.controls}>
+          <div
+            className={styles.backSection}
+            onMouseDown={() => {
+              ref.current.style.clipPath = `polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 25% 50%, 0% 0%)`;
+              window.location.href = `/`;
+            }}
+          >
+            <div ref={ref} />
+          </div>
+          <div className={styles.menuButton}>
+            <Hamburger color="#FFF" toggle={setOpen} toggled={isOpen} />
+          </div>
         </div>
-      </div>
-      <div className={styles.root}>
-        {children}
-        <div className={styles.footer}>
-          <Footer />
+      </>
+      <>
+        <div
+          className={styles.menuBase}
+          style={{
+            width: `${isOpen ? 100 : 0}%`,
+          }}
+        />
+        <div
+          className={styles.menu}
+          style={{
+            marginLeft: `${isOpen ? 0 : 100}%`,
+          }}
+        />
+      </>
+      <>
+        <div className={styles.root}>
+          {children}
+          <div className={styles.footer}>
+            <Footer />
+          </div>
         </div>
-      </div>
+      </>
     </>
   );
 }
