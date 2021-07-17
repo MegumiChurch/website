de87b162cb5422a25ad896681110f5ecfe832ab2
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-07-17 07:49:10 -0400

    fix(footer): fix footer link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,17 +7,27 @@ export default function Footer() {
         <div className={styles.inner}>
           <ul>
             <li>ニューヨークめぐみ教会</li>
-            <li>住所</li>
+            <li>
+              <a href='https://goo.gl/maps/tZb7swVVmkXuFxC27'>住所</a>
+            </li>
           </ul>
           <ul>
             <li>牧師・笹川雅弘</li>
             <li>Masahiro Sasakawa</li>
-            <li>メール</li>
+            <li>
+              <a href='mailto:msasakawa@ridgewaychurch.com'>メール</a>
+            </li>
           </ul>
           <ul>
-            <li>免責</li>
-            <li>プライバシーポリシー</li>
-            <li>利用規約</li>
+            <li>関連サイト</li>
+            <li>
+              <a href='https://nymissionsasakawa.wixsite.com/support'>
+                笹川雅弘宣教師を支える会
+              </a>
+            </li>
+            <li>
+              <a href='https://twitter.com/SasakawaM'>Twitter</a>
+            </li>
           </ul>
         </div>
         <div className={styles.copywrite}>
