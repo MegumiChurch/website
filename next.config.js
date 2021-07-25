e6fb8816cbe25a0500374a72fab7a5aa60654ac8
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-07-25 15:19:17 -0400

    feat(legacy): add legacy website

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-};
+  rewrites: async () => {
+    return [
+      {
+        source: '/legacy_website',
+        destination: '/legacy_website/index.html'
+      }
+    ]
+  }
+}
