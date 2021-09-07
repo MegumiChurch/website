93d93346bcd2cdaa05ff6276ccaa38db156cd29d
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-09-06 22:08:05 -0400

    feat: add auto-zoom fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react'
 import type { AppProps } from 'next/app'
 import 'styles/global.scss'
 import 'styles/font.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [mag, setMag] = useState(1)
+  useEffect(() => {
+    function handleResize() {
+      setMag(1.4 - 0.4 * ((window.outerWidth - 10) / window.innerWidth))
+    }
+    window.addEventListener(`resize`, handleResize)
+    return () => window.removeEventListener(`resize`, handleResize)
+  }, [])
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-size: ${mag}em;
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
